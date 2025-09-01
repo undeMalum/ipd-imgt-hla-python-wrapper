644f4c7622b1e959d488dd851b546e29afbea2f4
Mateusz Konat <polakwmc@gazeta.pl>
2025-09-01 19:53:51 +0200

    First complex interaction with the API

--- ipd_imgt_hla_python_wrapper/test_db_connection.py
+++ ipd_imgt_hla_python_wrapper/test_db_connection.py
@@ -6,5 +6,20 @@ app = FastAPI()
 
 @app.get('/')
 async def connect_to_db():
-    response = httpx.get('https://www.ebi.ac.uk/cgi-bin/ipd/api/allele?query=startsWith(name, "B*27")')
+    URL = 'https://www.ebi.ac.uk/cgi-bin/ipd/api/allele?query=startsWith(name, "B*27")'
+    
+    response = httpx.get(URL).json()
+    
+    next_page = response["meta"]["next"]
+    
+    while next_page is None:
+        temp_response = httpx.get(URL + next_page).json()
+        
+        print(temp_response)
+        
+        response["data"].extend(temp_response["data"])
+        
+        next_page = temp_response["meta"]["next"]
+    
+
     return response
