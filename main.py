3902fe1e7b4fb88651e076878d1bbc939bfe7e61
Mateusz Konat <polakwmc@gazeta.pl>
2025-09-03 14:02:01 +0200

    Finish refactoring code

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from ipd-imgt-hla-python-wrapper!")
+from fastapi import FastAPI
+
+from ipd_imgt_hla_python_wrapper.services.allele_services import (
+    fetch_all_alleles_from_query,
+    download_alleles,
+)
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def get_all_alleles():
+    query = 'startsWith(name, "B*27")'
+    data = await fetch_all_alleles_from_query(query)
+
+    return data
+
+
+@app.get()
+async def get_allele_sequences():
+    query = 'startsWith(name, "B*27")'
+    data = await download_alleles(query)
+
+    return data
