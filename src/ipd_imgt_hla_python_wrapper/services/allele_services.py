5cacbaaf82bd15bd93b0610f3e18f33c58d991c1
Mateusz Konat <polakwmc@gazeta.pl>
2025-09-04 14:45:05 +0200

    finish a possiblity of downloading more than 1000 alleles

--- src/ipd_imgt_hla_python_wrapper/services/allele_services.py
+++ src/ipd_imgt_hla_python_wrapper/services/allele_services.py
@@ -53,10 +53,31 @@ def retrieve_allele_accession_numbers(allele_names: AllelesNames) -> list[str]:
     return allele_accession_list
 
 
-def donwload_over_1000_alele(
+async def donwload_over_1000_alele(
     allele_accession_list: list[str], seq_type: SequenceTypes = SequenceTypes.GENOMIC
 ) -> AllelesSequences:
-    pass
+    sequences = []
+    
+    async with httpx.AsyncClient(timeout=60.0) as client:
+        for allele in allele_accession_list:
+            single_allele_url = f"{ALLELE_URL}/{allele}"
+            response = await client.get(single_allele_url)
+            
+            allele_info = response.json()
+            
+            status = allele_info["status"]
+            
+            if status == "Deleted":
+                continue
+            
+            sequence = allele_info["sequence"].get(f"{seq_type.value}")
+        
+            if sequence is None:
+                continue
+            
+            sequences.append({"allele_name": allele_info["name"], "sequence": sequence})
+    
+    return {"sequences": sequences}
 
 
 if __name__ == "__main__":
@@ -67,4 +88,6 @@ if __name__ == "__main__":
     data = asyncio.run(fetch_all_alleles_from_query(query))
 
     accession_list = retrieve_allele_accession_numbers(data)
-    print(accession_list)
+    
+    downloaded_sequences = asyncio.run(donwload_over_1000_alele(accession_list))
+    print(downloaded_sequences)
