cc05f3feac46315bd8f702d9900e14672b172540
Mateusz Konat <polakwmc@gazeta.pl>
2025-09-10 22:32:47 +0200

    start playing with marimo as a web app

---
+++ sundbox/Tryout using marimo as a web app.py
@@ -0,0 +1,19 @@
+import marimo
+
+__generated_with = "0.15.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return
+
+
+if __name__ == "__main__":
+    app.run()
