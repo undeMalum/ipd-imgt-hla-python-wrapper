daed7656e84073a3b362973746765c1748f7966e
Mateusz Konat <polakwmc@gazeta.pl>
2025-09-16 15:59:47 +0200

    add new interface to the main function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import FastAPI
+import httpx
 
 from src.ipd_imgt_hla_python_wrapper.services.allele_services import (
     fetch_all_alleles_from_query,
@@ -14,39 +15,44 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("ipd_hla_wrapper.log"), logging.StreamHandler()],
 )
-loggger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def get_all_alleles():
-    loggger.info("GET '/' endpoint called.")
+    logger.info("GET '/' endpoint called.")
 
     query = 'startsWith(name, "B*27")'
-    data = await fetch_all_alleles_from_query(query)
+    
+    async with httpx.AsyncClient() as client:
+        data = await fetch_all_alleles_from_query(client, query)
 
     return data
 
 
 @app.get("/downloads")
 async def get_allele_sequences():
-    loggger.info("GET '/downloads' endpoint called.")
+    logger.info("GET '/downloads' endpoint called.")
 
     query = 'startsWith(name, "B*27")'
-    data = await download_alleles(query)
+    
+    async with httpx.AsyncClient() as client:
+        data = await download_alleles(client, query)
 
     return data
 
 
 @app.get("/downloads/large")
 async def get_large_number_sequences():
-    loggger.info("GET '/downloads/large' endpoint called.")
+    logger.info("GET '/downloads/large' endpoint called.")
 
     query = 'startsWith(name, "B")'
 
-    allele_names = await fetch_all_alleles_from_query(query)
-    allele_list = retrieve_allele_accession_numbers(allele_names)
-    data = await download_over_1000_alleles(allele_list)
+    async with httpx.AsyncClient() as client:
+        allele_names = await fetch_all_alleles_from_query(query)
+        allele_list = retrieve_allele_accession_numbers(allele_names)
+        data = await download_over_1000_alleles(allele_list)
 
     return data
